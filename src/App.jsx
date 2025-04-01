95b9aa9139e6ce6a51842223969e88dad0956688
moharafat <mohamedamr0001@gmail.com>
2025-04-01 14:27:19 +0200

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import Header from "./Components/Header";
 import MainContent from "./Components/MainContent";
 import Footer from "./Components/Footer";
 import WelcomeMessage from "./Components/WelcomeMessage";
+import JSXRules from "./Components/JSXRules";
 
 const App = () => {
   return (
-    <div>
-      <WelcomeMessage />
-    </div>
+    <section id="section">
+      <form>
+        <label htmlFor="acoount_number">account number</label>
+        <input type="text" placeholder="CX Account #" />
+        <JSXRules></JSXRules>
+      </form>
+    </section>
   );
 };
 export default App;
---
+++ src/Components/JSXRules.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const JSXRules = () => {
+  return (
+    <div>
+      <h1>JSX Rules</h1>
+      <p className="RULES">
+        <ul>capital</ul>
+        <ul>not html</ul>
+        <ul>capital</ul>
+      </p>
+    </div>
+  );
+};
+
+export default JSXRules;
