b69304beb47852cdaa08484620c3f73876c72f07
moharafat <mohamedamr0001@gmail.com>
2025-04-01 10:58:14 +0200

    JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import Greet from "./Components/Greet";
 import Header from "./Components/Header";
 import MainContent from "./Components/MainContent";
 import Footer from "./Components/Footer";
+import WelcomeMessage from "./Components/WelcomeMessage";
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <WelcomeMessage />
     </div>
   );
 };
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Header = () => {
-  return <h1>Welcome to My website</h1>;
-  <nav>
-    <a>Home</a>
-    <a>About</a>
-    <a>Contact</a>
-  </nav>;
+  return (
+    <div>
+      <h1>Welcome to My website</h1>
+      <nav>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
