541a427b2782633af35c74e142a6392cc18b4dc4
moharafat <mohamedamr0001@gmail.com>
2025-04-01 18:00:07 +0200

    dad

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
-import Add from "./Components/Add";
-import Greet from "./Components/Greet";
-import Header from "./Components/Header";
-import MainContent from "./Components/MainContent";
-import Footer from "./Components/Footer";
-import WelcomeMessage from "./Components/WelcomeMessage";
-import JSXRules from "./Components/JSXRules";
+import Greet from "./Components/Greeting";
+import ProductInfo from "./Components/ProductInfo";
 
 const App = () => {
   return (
-    <section id="section">
-      <form>
-        <label htmlFor="acoount_number">account number</label>
-        <input type="text" placeholder="CX Account #" />
-        <JSXRules></JSXRules>
-      </form>
-    </section>
+    <div>
+      {/* <label htmlFor="acoount_number">account number</label>
+        <input type="text" placeholder="CX Account #" /> */}
+      <Greet />
+      <ProductInfo />
+    </div>
   );
 };
 export default App;
--- src/Components/Greet.jsx
+++
@@ -1,4 +0,0 @@
-const Greet = () => {
-    return <h1>Hello_World</h1>
-}
-export default Greet;
