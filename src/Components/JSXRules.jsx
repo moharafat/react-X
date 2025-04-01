3a3d0404a330b0cb13836a229c37f8ebe3e4c170
moharafat <mohamedamr0001@gmail.com>
2025-04-01 18:00:00 +0200

    da

--- src/Components/JSXRules.jsx
+++ src/Components/JSXRules.jsx
@@ -4,11 +4,9 @@ const JSXRules = () => {
   return (
     <div>
       <h1>JSX Rules</h1>
-      <p className="RULES">
         <ul>capital</ul>
         <ul>not html</ul>
         <ul>capital</ul>
-      </p>
     </div>
   );
 };
