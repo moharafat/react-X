73fdc38feccf5939bacdb36eeda2eead47c53a40
moharafat <mohamedamr0001@gmail.com>
2025-04-06 16:48:31 +0200

    Children

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Person from "./Components/Person";
-import Products from "./Components/Products";
+import React from "react";
+import Card from "./Components/Card";
 
 const App = () => {
   return (
     <div>
-      <Person name="7amo" age={23} />
-      <Products name="iphone" price={33} />
+      <Card>
+        <h1> My card </h1>
+        <p>This is some content for card1</p>
+      </Card>
     </div>
   );
 };
