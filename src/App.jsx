760a7603972ad16d8bd1864d061cbcd74c04b62d
moharafat <mohamedamr0001@gmail.com>
2025-04-09 12:43:46 +0200

    Profilecard

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { PiTextbox } from "react-icons/pi";
 import React from "react";
 import "./index.css";
 import StyledCard from "./Components/StyledCard";
+import ProfileCard from "./Components/ProfileCard";
 
 const App = () => {
   return (
     <section>
       <StyledCard />
+      <ProfileCard />
     </section>
   );
 };
