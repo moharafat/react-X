c433a39e85de7c1c3ea781e087cbe9ad2c990c8f
moharafat <mohamedamr0001@gmail.com>
2025-04-10 14:06:29 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { PiTextbox } from "react-icons/pi";
 import React from "react";
 // import "./index.css";
 import StyledCard from "./Components/StyledCard";
+import ProfileCard from "./Components/ProfileCard";
 function App() {
   return (
     <div>
       <StyledCard />
+      <ProfileCard />
     </div>
   );
 }
--- src/Components/ProfileCard.jsx
+++ src/Components/ProfileCard.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const ProfileCard = () => {
+  const styles = {
+    background: "lightgray",
+    padding: "15px",
+    borderRadius: "8px",
+    color: "black",
+  };
   return (
-    <div>ProfileCard</div>
-  )
-}
+    <div style={styles}>
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum
+        praesentium ut aut. Deleniti magni explicabo, praesentium, sed
+        recusandae facere culpa vel a, quidem accusamus iste nesciunt quasi enim
+        delectus quia.
+      </p>
+    </div>
+  );
+};
 
-export default ProfileCard
+export default ProfileCard;
