d8d89f02ba10ad102251a0c4e3858ae0ef992e3e
moharafat <mohamedamr0001@gmail.com>
2025-04-15 09:13:34 +0200

    other way of writing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import { useState } from "react";
 
 const App = () => {
-  const [friends, setFriends] = useState(["Alex", "john"]);
-  const addOneFriend = () => setFriends([...friends, "7ammmo"]);
-  const removeFriend = () => setFriends(friends.filter((f) => f != "john"));
-  const update1Friend = () => {
-    setFriends(friends.map(f => f=== 'Alex' ? 'Alex Smith' : f))
-    }
-  
+  const [movie, setMovie] = useState({
+    title: "Equalizer 3",
+    ratings: 7,
+  });
+
+  const handleClick = () => {
+    // const copyMovie = {
+    //   ...movie,
+    //   ratings: 5,
+    // };
+    setMovie({ ...movie, ratings: 6 });
+  };
+
   return (
     <section>
-      {friends.map((f) => (
-        <li key={Math.random()}>{f}</li>
-      ))}
-      <button onClick={addOneFriend}>Add new Friend</button>
-      <button onClick={removeFriend}>Remove john</button>
-      <button onClick={update1Friend}>update 1 friend</button>
+      <h1>Movie:{movie.title}</h1>
+      <p>Rating: {movie.ratings}</p>
+      <button onClick={handleClick}>CHANGE THE RATING</button>
     </section>
   );
 };
-
 export default App;
