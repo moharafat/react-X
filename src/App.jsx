1af71af9eac46fc8dbca834a31f882b2b9d86cb4
moharafat <mohamedamr0001@gmail.com>
2025-04-22 16:06:13 +0200

    change movie name by using ID

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 const App = () => {
   const [movies, setMovies] = useState([
-    { id: 1, title: "Spiderman" },
-    { id: 2, title: "batman" },
+    { id: 1, title: "Spiderman", ratings: 3 },
+    { id: 2, title: "batman", ratings: 5 },
   ]);
   const handleClick = () => {
-    setMovies(movies.map(m =>))
-  }
+    setMovies(
+      movies.map((m) =>
+        m.id === 1 ? { ...movies, title: "7ammo el5ateer" } : m
+      )
+    );
+  };
   return (
     <section>
       {movies.map((m) => (
-        <li key={Math.random}>{m.title}</li>
+        <li key={Math.random()}>{m.title}</li>
       ))}
 
       <button onClick={handleClick}>Changename</button>
