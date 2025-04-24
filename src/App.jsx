23963136ddff9b3cb5cc9b65306fe206a3e176fc
moharafat <mohamedamr0001@gmail.com>
2025-04-24 12:13:46 +0200

    EX3

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import { useState } from "react";
-const App = () => {
-  const [movies, setMovies] = useState([
-    { id: 1, title: "Spiderman", ratings: 3 },
-    { id: 2, title: "batman", ratings: 5 },
-  ]);
-  const handleClick = () => {
-    setMovies(
-      movies.map((m) =>
-        m.id === 1 ? { ...movies, title: "7ammo el5ateer" } : m
-      )
-    );
-  };
-  return (
-    <section>
-      {movies.map((m) => (
-        <li key={Math.random()}>{m.title}</li>
-      ))}
+import ExampleThree from "./Components/ExampleThree";
 
-      <button onClick={handleClick}>Changename</button>
-    </section>
+const App = () => {
+  return (
+    <div>
+      <ExampleThree />
+    </div>
   );
 };
 export default App;
