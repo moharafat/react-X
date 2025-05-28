a36227b239608973e407e09f7daad57c2a3ad3fb
moharafat <mohamedamr0001@gmail.com>
2025-05-28 19:28:42 +0300

    use reff 11st

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,18 @@
-// import { useReducer } from "react";
+import { useRef } from "react";
+const App = () => {
+  const inputElement = useRef(null);
 
-// const initialState = { count: 0 };
+  const focusInput = () => {
+    inputElement.current.focusInput();
+    inputElement.current.value="7amo"
 
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case "increment":
-//       return { ...state, count: state.count + 1 };
-//     case "decrement":
-//       return { ...state, count: state.count - 1 };
-//       break;
-//     case "reset":
-//       return { ...state, count: 0 };
-
-//     default:
-//       break;
-//   }
-// };
-
-// const App = () => {
-//   const [state, dispatch] = useReducer(reducer, initialState);
-//   return (
-//     <div>
-//       <button onClick={() => dispatch({ type: "increment" })}>+</button>
-//       <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-//       <button onClick={() => dispatch({ type: "reset" })}>RESET</button>
-
-//       <h1>count is {state.count}</h1>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-import React from "react";
-import Counter from "./Components/Counter";
-
-function App() {
+  };
   return (
     <div>
-      <h1>React</h1>
-      <Counter />
+      <input type="text" ref={inputElement} />
+      <button onClick={() => focusInput()}>Focus</button>
     </div>
   );
-}
+};
 
 export default App;
